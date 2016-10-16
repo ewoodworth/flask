c1b4fb2b142085ed61ecfa65433d2ca867fe5b8a
vagrant <vagrant@vagrant.vm>
2016-10-16 22:36:06 +0000

    Refactored with POST method. Oops.

--- application.py
+++ application.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     """Show an index page."""
 #Render the template created in the html/css assesment onto this route   
     return render_template("application-form.html")
 
-@app.route("/application")
+@app.route("/application", methods=['GET', 'POST'])
 def response_page():
     """Show the user a response to their application"""
     #Render the info collected in the application form to a Jinja template onto this route
     #Get responses from form, assign them variable names
-    firstname = request.args.get("firstname")
-    lastname = request.args.get("lastname")
-    jobtitle = request.args.get("jobtitle")
-    salaryrequirement = request.args.get("salaryrequirement")
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    jobtitle = request.form.get("jobtitle")
+    salaryrequirement = request.form.get("salaryrequirement")
     salaryrequirement = str(salaryrequirement)
 
 #Rnder the responses into the response template
@@ -31,7 +31,7 @@ def response_page():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = False
+    app.debug = True
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
